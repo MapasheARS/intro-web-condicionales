051e144be41a7a2d350ac24b5f02642ca4c765e7
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-11-13 08:22:05 -0600

    Actualizacion, condicionales

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ let birdBottom = 100
 Crea una función terminar(). El siguiente código se debe de ejecutar al llamar a la función:
 clearInterval(timerId)
 */
-
+function terminar(){
+    clearInterval(timerId);
+}
 
 /* Ejercicio 2.
 Crea una función verificarColision(). Dentro de la función, agrega una declaración if
 que revise si birdBottom es menor a 0. Si es birdBottom es menor a 0, llama a la función terminar().
 */
+function verificarColision(){
+    if(birdBottom < 20){
+        terminar();
+    };
+}
 
 
 
@@ -23,6 +30,7 @@ Modifica la función loop(). Cuando se llame a la función:
 */
 function loop() {
     efectoGravedad() 
+    verificarColision()
 }
 
 
